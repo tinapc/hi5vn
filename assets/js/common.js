c9bf5f21194dde8b636c75790b6da2162c35786c
tinapc <tinapc@gmail.com>
2014-10-16 00:02:38 +0700

    Orignal source

---
+++ assets/js/common.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+	//Show/Hide List Choose Places
+	$('.drop-down-list-places').click(function(){
+		$('#list-choose-places').slideToggle();
+		$('#list-choose-places').toggleClass('expand');
+	});
+
+});
